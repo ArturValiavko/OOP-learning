62863c98e1f822884cd2a9b6a8ab05e9899577f8
Artur Valiavko <Artas840@gmail.com>
2025-07-06 19:06:16 +0300

    Task0 - OOP class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class žmogus //Task0
+{
+    public string name = "";
+    public int age;
+
+    public void Introduce()
+    {
+        Console.WriteLine($"Sveiki, mano vardas {name}, man yra {age} metų");
+     }
+  }
+
+class Programs
+{
+    static void Main()
+    {
+        žmogus persone1 = new žmogus();
+        žmogus persone2 = new žmogus();
+        persone1.name = "Artur";
+        persone2.name = "Tomas";
+        persone1.age = 29;
+        persone2.age = 33;
+
+        persone1.Introduce();
+        persone2.Introduce();
+    }
+
+  }
