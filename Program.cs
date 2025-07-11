0aeee5f2f100e8b68706c1f7dc7648d03175946a
Artur Valiavko <Artas840@gmail.com>
2025-07-11 22:54:22 +0300

    Task6 - list/while solo task.

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel;
+using System.IO.Pipes;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 
-class Zmogus //Task5  
+class Zmogus //Task6
 {
     public string Name { get; set; }
     public int Age { get; set; }
@@ -41,26 +43,38 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Įveskite vardą: ");
-        string? name = Console.ReadLine();
-        if (string.IsNullOrWhiteSpace(name))
-        {
-            name = "Nežinomas";
-        }
-        Console.Write("Įveskit amžių: ");
-        int age;
-
-        if (!int.TryParse(Console.ReadLine(), out age))
-        {
-            Console.WriteLine("Neteisingas amžius");
-            age = 0;
-        }
-
-        Zmogus zmogus = new Zmogus(name, age);
-        Console.WriteLine(zmogus.GetInfo());
+        List<Zmogus> zmones = new List <Zmogus>();
         
+        while (true)
+        {
+
+            Console.Write("Įveskite vardą: ");
+            string? name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                break;
+            }
+            Console.Write("Įveskit amžių: ");
+            int age;
+
+            if (!int.TryParse(Console.ReadLine(), out age))
+            {
+                Console.WriteLine("Neteisingas amžius");
+                age = 0;
+            }
+            //Zmogus zmogus = new Zmogus(name, age);
+            zmones.Add(new Zmogus(name, age));
+
+        }
+        Console.WriteLine("\n--- Rezultatai ---\n");
+        foreach (Zmogus z in zmones)
+        {
+            Console.WriteLine(z.GetInfo());
+        }
+        
+        }
     
     }
     
 
-  }
+  
