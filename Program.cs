a9c79c6444a638545933abbb89c1c6a6e305192b
Artur Valiavko <Artas840@gmail.com>
2025-07-14 00:22:43 +0300

    Task 8.1 padarytas

--- Program.cs
+++ Program.cs
@@ -6,6 +6,66 @@ using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
 using System.Linq;
 
-    
+class Zmogus //Task8.1
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
 
-  
+    public Zmogus(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    public string KoksAm()
+    {
+        if (Age == 0)
+        {
+            return $"Vardas{Name}, amžius Nenurodytas";
+        }
+        else
+        {
+            return $"Vardas{Name}, amžius yra {Age}";
+        }
+    }
+ }
+
+
+class Program
+{
+    static void Main()
+    {
+        List<Zmogus> zmones = new List<Zmogus>();
+
+        while (true)
+        {
+            Console.Write("Įveks vardą:");
+            string? name = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                break;
+            }
+            Console.Write("Įvesk amžių:");
+            if (!int.TryParse(Console.ReadLine(), out int age))
+            {
+                Console.WriteLine("Neteisingas amžius");
+                age = 0;
+            }
+            zmones.Add(new Zmogus(name, age));
+
+        }
+        var filtr = zmones.Where(a => a.Age > 17).ToList();
+
+        if (filtr.Count == 0)
+        {
+            Console.WriteLine("sarašė nėra 18+");
+        }
+        else
+        { 
+           foreach (Zmogus abc in filtr)
+           {
+                Console.WriteLine(abc.KoksAm());            
+           }
+        }
+    }
+    
+}
