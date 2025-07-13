c32df372a9190e68889e609ef565ac04af68c7ad
Artur Valiavko <Artas840@gmail.com>
2025-07-13 23:23:42 +0300

    Task8 sarašo tikrinimas .Where (x => x.Age)

--- Program.cs
+++ Program.cs
@@ -4,8 +4,9 @@ using System.IO.Pipes;
 using System.Reflection;
 using System.Reflection.Metadata;
 using System.Reflection.Metadata.Ecma335;
+using System.Linq;
 
-class Zmogus //Task6
+class Zmogus //Task8
 {
     public string Name { get; set; }
     public int Age { get; set; }
@@ -21,23 +22,20 @@ class Zmogus //Task6
         }
         Age = age;
     }
-      public string GetInfo()
+    public string GetInfo()
     {
         if (Age == 0)
         {
-            return $"Mano vardas {Name}, amžius Nenurodytas";
+            return $"Vardas {Name}, amžius Nenurodytas";
         }
         else
         {
-            return $"Mano vardas {Name}, man yra {Age} metų";
+            return $"Vardas {Name}, amžius yra {Age}";
         }
 
 
     }
 }    
-
-    
-
 
 class Program
 {
@@ -66,15 +64,26 @@ class Program
             zmones.Add(new Zmogus(name, age));
 
         }
+        var filtruoti = zmones.Where(a => a.Age > 17).ToList();
+
         Console.WriteLine("\n--- Rezultatai ---\n");
-        foreach (Zmogus z in zmones)
+
+
+        if (filtruoti.Count == 0)
         {
-            Console.WriteLine(z.GetInfo());
+            Console.WriteLine("Sąraše nėra pilnamečių.");
         }
-        
-        }
-    
+        else
+        {
+            foreach (Zmogus zmogus in filtruoti)
+            {
+                Console.WriteLine(zmogus.GetInfo());
+
+            }
+        }     
     }
+    
+}
     
 
   
