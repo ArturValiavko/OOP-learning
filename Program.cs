a63f0d33c50be8427d69e80d0f45d2ef6b5ad648
Artur Valiavko <Artas840@gmail.com>
2025-08-12 18:18:45 +0300

    Task 17.1 Sandelys

--- Program.cs
+++ Program.cs
@@ -1,39 +1,84 @@
 ﻿using System;
+using System.Data;
+using System.Dynamic;
 
 
 //task 17 events 
 
-
-public class TemperaturosDaviklis
+public class KiekioEventArgs : EventArgs
 {
-    public event Action<int> TemperaturaPakyla;
-    private int temperatura;
+    public string Pavadinimas { get; }
+    public int Kiekis { get; }
+    public int Riba { get; }
 
-    public void NUstatytitemperatura(int nuajaTemp)
+    public KiekioEventArgs(string pavadinimas, int kiekis, int riba)
     {
-        temperatura = nuajaTemp;
-        Console.WriteLine($"Nustatyta temperatūra: {temperatura} °C");
+        Pavadinimas = pavadinimas;
+        Kiekis = kiekis;
+        Riba = riba;
+    }
+}
+public class Sandelys
+{
+    public string Pavadinimas { get; }
+    public int Kiekis { get; private set; }
+    public int Riba { get; }
 
-        if (temperatura > 25)
+    public event EventHandler<KiekioEventArgs>? KiekisNukritoZemiauRibos;
+
+    public Sandelys(string pavadinimas, int pradinisKiekis, int riba)
+    {
+        Pavadinimas = pavadinimas;
+        Kiekis = pradinisKiekis;
+        Riba = riba;
+    }
+    public void Paimk(int kiekis)
+    {
+        int buvo = Kiekis;
+        Kiekis += kiekis;
+        Console.WriteLine($"[Sandėlys] '{Pavadinimas}': {kiekis}, kiekis dabar {Kiekis} ");
+
+        if (buvo >= Riba && Kiekis < Riba)
         {
-            TemperaturaPakyla?.Invoke(temperatura);
+            KiekisNukritoZemiauRibos?.Invoke(this, new KiekioEventArgs(Pavadinimas, Kiekis, Riba));
         }
     }
-
+}
+public static class Prenumeratoriai
+{
+    public static void IspetiKonsolėje(object? sender, KiekioEventArgs e)
+    {
+        Console.WriteLine($"ĮSPEJIMAS: '{e.Pavadinimas}' kiekis {e.Kiekis} < riba {e.Riba}");
+    }
+    public static void SiustiElLaiška(object? sender, KiekioEventArgs e)
+    {
+        Console.WriteLine($"[Email] Tema: Mažas kiekis - {e.Pavadinimas}. Kiekis: {e.Kiekis}.");
+    }
 }
 
 class Program
 {
     static void Main()
     {
-        TemperaturosDaviklis daviklis = new TemperaturosDaviklis();
+        var morka = new Sandelys("Morka", pradinisKiekis: 15, riba: 15);
+        var bulvė = new Sandelys("Bulvė", pradinisKiekis: 8, riba: 5);
 
-        daviklis.TemperaturaPakyla += temp =>
-        {
-            Console.WriteLine($"Įspejimas: temperatūra per aukšta! ({temp}°C)");
-        };
-        daviklis.NUstatytitemperatura(20);
-        daviklis.NUstatytitemperatura(21);
+        morka.KiekisNukritoZemiauRibos += Prenumeratoriai.IspetiKonsolėje;
+        morka.KiekisNukritoZemiauRibos += Prenumeratoriai.SiustiElLaiška;
+
+        bulvė.KiekisNukritoZemiauRibos += (s, e) =>
+        Console.WriteLine($"LOG: {e.Pavadinimas} nukrito žemiau ribos ({e.Kiekis}/{e.Riba})");
+
+        bulvė.KiekisNukritoZemiauRibos += Prenumeratoriai.IspetiKonsolėje;
+        bulvė.KiekisNukritoZemiauRibos += Prenumeratoriai.SiustiElLaiška;
+
+        morka.Paimk(-3);
+        morka.Paimk(5);
+
+        bulvė.Paimk(-5);
+        bulvė.Paimk(3);
+
+        morka.KiekisNukritoZemiauRibos -= Prenumeratoriai.SiustiElLaiška;
+        morka.Paimk(1);
     }
-
 }
