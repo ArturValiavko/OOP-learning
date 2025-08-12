daa8b00bca1aa03b4f95e0c66c898a8c9725be0a
Artur Valiavko <Artas840@gmail.com>
2025-08-12 13:46:30 +0300

    Task 16 Delegatai

--- Program.cs
+++ Program.cs
@@ -2,314 +2,128 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
+using Microsoft.VisualBasic;
 
-public static class FileHelper
-{
-    private static string Filepath = "duomenys.json";
-    public static void SaveToFile(List<Zmogus> zmones)
-    {
-        var options = new JsonSerializerOptions { WriteIndented = true };
-        string json = JsonSerializer.Serialize(zmones, options);
-        File.WriteAllText(Filepath, json);
-    }
-
-    public static List<Zmogus> LoadFromFile()
-    {
-        if (!File.Exists(Filepath))
-        {
-            return new List<Zmogus>();
-        }
-        string json = File.ReadAllText(Filepath);
-        return JsonSerializer.Deserialize<List<Zmogus>>(json);
-    }
-
-}
-public class Zmogus // Task 13
-{
-    public string Name { get; set; }
-    public string Sname { get; set; }
-    public int Age { get; set; }
-    public int Id { get; set; }
-
-    public Zmogus(int id, string name, string sname, int age) // KONSTRUKTAS 
-    {
-        Name = name;
-        Sname = sname;
-        Age = age < 0 ? 0 : age;
-        Id = id;
-
-
-    }
-
-    protected virtual string GetInfo() //Metodas 1 
-    {
-        if (Age == 0)
-        {
-            return $" {Id} | Vardas - {Name}, pavardė - {Sname}, amžius nenurodytas";
-        }
-        else
-        {
-            return $" {Id} | Vardas - {Name}, pavardė - {Sname}, amžius - {Age}";
-        }
-
-    }
-
-    public virtual void Print()
-    {
-        Console.WriteLine(GetInfo());
-    }
-}
-
-public class Studentas : Zmogus
-{
-    public string Universitetas { get; set; }
-
-    public Studentas(int id, string name, string sname, int age, string universitetas)// KONSTRUKTAS  2
-    : base(id, name, sname, age)
-    {
-        Universitetas = universitetas;
-    }
-    protected override string GetInfo() //Metodas 2
-    {
-        string baseInfo = base.GetInfo();
-        if (Age != 0)
-        {
-            baseInfo += $", universitetas - {Universitetas}";
-        }
-        return baseInfo;
-    }
-}
+//Task16
+public delegate string TekstoOperacija(string input);
+public delegate string SkaiciuOperacija(int x, int y);
+public delegate int SkaiciuSarašoOperacija(int input);
 
 class Program
 {
-    public static void Main() // METODAS
+    static string IDidziasias(string s) => s?.ToUpper() ?? ""; //TekstoOperacija
+    static string IMazosiomis(string s) => s?.ToLower() ?? "";
+    static string Atbulai(string s)
     {
-        List<Zmogus> abc = FileHelper.LoadFromFile();//abc tai sara6o pavadinimas kuriame yra  
-        int id = abc.Count > 0 ? abc.Max(z => z.Id) : 0;
-        while (true)
-        {
-
-            id++;
-            Console.Write("Įvesk vardą: ");
-            string? name = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(name))
-            {
-                break;
-            }
-
-            Console.Write("Įvesk pavardę: ");
-            string? sname = Console.ReadLine();
-            if (string.IsNullOrWhiteSpace(sname))
-            {
-                sname = "Nėra";
-            }
-            Console.Write("Amžių: ");
-            if (!int.TryParse(Console.ReadLine(), out int age))
-            {
-                Console.WriteLine("Neteisingai įvestas amžius, priskiriamas 0");
-                age = 0;
-            }
-            Console.WriteLine("Ar įvestas žmogus studentas?(y/n)");
-            string? arstud = Console.ReadLine();
-            bool yrastud = arstud != null && arstud.Trim().ToLower() == "y";
-
-            if (yrastud)
-            {
-                Console.Write("Įvesk universiteto pavadinimą: ");
-                string? universitetas = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(universitetas))
-                {
-                    universitetas = "Nežinomas";
-                }
-                abc.Add(new Studentas(id, name, sname, age, universitetas));
-            }
-            else
-            {
-                abc.Add(new Zmogus(id, name, sname, age));
-
-            }
-            FileHelper.SaveToFile(abc);
-        }
-
-        while (true)
-        {
-            Console.WriteLine("SARAŠO VALDYMAS  \n 1 - Surasti žmogu sarašė \n 2 - Vidutinis amžius sarašė \n 3 - Surūšiuota pagal ID MaxMin \n 4 - Surūšiuota pagal ID MinMAx \n 5 - Visas sąrašas: \n 6 - Ištrinti įrašą iš sarašo: \n 7 - Redaguoti įrašą pagal ID:  ");
-            Console.Write("Pasirink skaičiu 1-7: (Enter - išeiti) ");
-            if (int.TryParse(Console.ReadLine(), out int pasirinkimas))
-            {
-                switch (pasirinkimas)
-                {
-                    case 1:
-                        if (abc.Count > 0)
-                        {
-                            while (true)
-                            {
-                                Console.Write("\n Kokio žmogaus ieškote?: (Enter to skip) ");
-                                string? ieskomasVardas = Console.ReadLine();
-                                if (string.IsNullOrWhiteSpace(ieskomasVardas))
-                                {
-                                    break;
-                                }
-                                var rasti = abc.Where(z => z.Name.Equals(ieskomasVardas, StringComparison.OrdinalIgnoreCase)).ToList();
-                                if (rasti.Count > 0)
-                                {
-                                    foreach (Zmogus a in rasti)
-                                    {
-                                        a.Print();
-                                    }
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Tokio žmogaus nėra \n");
-                                    continue;
-                                }
-
-                                break;
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Sarašas tuščias");
-                        }
-                        break;
-
-
-                    case 2:
-                        double vidurkis = abc.Average(z => z.Age);
-                        Console.WriteLine($"Amžiaus vidurkis = {vidurkis:F1} \n Visas sarašas: ");
-
-                        break;
-
-                    case 3:
-
-                        var idmaxmin = abc.OrderByDescending(z => z.Id).ToList();
-                        Console.WriteLine("\n ID - max to min ");
-                        foreach (Zmogus z in idmaxmin)
-                        {
-                            z.Print();
-                        }
-
-                        break;
-                    case 4:
-
-                        var idminmax = abc.OrderBy(z => z.Id).ToList();
-                        Console.WriteLine("\n ID - min to max ");
-                        foreach (Zmogus z in idminmax)
-                        {
-                            z.Print();
-                        }
-
-                        break;
-
-                    case 5:
-
-                        Console.WriteLine("\n Visas sąrašas  ");
-                        foreach (Zmogus z in abc)
-                        {
-                            z.Print();
-                        }
-                        break;
-
-                    case 6:
-
-                        Console.WriteLine("Įvesk ID kuri nori pašalinti");
-                        if (int.TryParse(Console.ReadLine(), out int IdRemove))
-                        {
-                            var zmogusToDelete = abc.FirstOrDefault(z => z.Id == IdRemove);
-                            if (zmogusToDelete != null)
-                            {
-                                abc.Remove(zmogusToDelete);
-                                FileHelper.SaveToFile(abc);
-                                Console.WriteLine("Žmogus ištrintas");
-                            }
-                            else
-                            {
-                                Console.WriteLine("ID nerastas");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine(" Tokio ID sąraše nėra");
-                        }
-                        break;
-
-                    case 7:
-
-                        Console.WriteLine("Įvesk ID kuri nori readaguoti");
-                        if (int.TryParse(Console.ReadLine(), out int Idredagavimas))
-                        {
-                            var zmogusRedaguoti = abc.Find(z => z.Id == Idredagavimas);
-                            if (zmogusRedaguoti != null)
-                            {
-                                Console.Write("Įvesk Nauja Vardą: ");
-                                string? name = Console.ReadLine();
-                                if (!string.IsNullOrWhiteSpace(name))
-                                {
-                                    zmogusRedaguoti.Name = name;
-                                    
-                                }
-                                Console.Write("Įvesk Nauja Pavardę: ");
-
-                                string? sname = Console.ReadLine();
-
-                                if (!string.IsNullOrWhiteSpace(sname))
-                                {
-                                    zmogusRedaguoti.Sname = sname;
-                                }
-
-                                Console.Write("Įvesk Naują Amžių: ");
-                                string? ageInput = Console.ReadLine();
-                                if (!string.IsNullOrWhiteSpace(ageInput))
-                                {
-
-                                    if (!int.TryParse(ageInput, out int age))
-                                    {
-                                        Console.WriteLine("Neteisingai įvestas amžius, priskiriamas 0");
-                                        age = 0;
-                                    }
-                                    zmogusRedaguoti.Age = age;
-                                    
-                                }
-                                if (zmogusRedaguoti is Studentas studentas)
-                                {
-                                    Console.Write("Įvesk Naują universiteto pavadinimą: ");
-                                    string? universitetas = Console.ReadLine();
-                                    if (!string.IsNullOrWhiteSpace(universitetas))
-                                    {
-                                        studentas.Universitetas = universitetas;
-                                    }
-                                }
-                                Console.WriteLine("Duomenis atnaujinti");
-                                FileHelper.SaveToFile(abc);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Tokio ID sąraše nėra");
-                            }
-                        }
-                        else
-                        {
-                            Console.WriteLine("Neteisingas ID formatas");
-                        }
-
-                        break;
-
-                    default:
-
-                        Console.WriteLine("Nebuvo pasirinktas sakičius");
-                        break;
-                }
-
-
-            }
-            else
-            {
-                break;
-            }
-
-        }
-
-
+        if (string.IsNullOrEmpty(s)) return "";
+        char[] arr = s.ToCharArray();
+        Array.Reverse(arr);
+        return new string(arr);
     }
+    static List<string> Pritaikyti(List<string> šaltinis, TekstoOperacija op)
+    {
+        var rezultatas = new List<string>(šaltinis.Count);
+        foreach (var e in šaltinis)
+            rezultatas.Add(op(e));
+        return rezultatas;
+    }
+
+    static string Sudetis(int a, int b) => $"{a}+{b}={a + b}"; //SkaiciuOperacija
+    static string Atimti(int a, int b) => $"{a}-{b}={a - b}";
+    static string Daugyba(int a, int b) =>$"{a}*{b}={a * b}";
+    static string Dalyba(int a, int b) => b == 0 ? "Dalyba iš nulio negalima!" : $"{a}/{b}={(double)a / b}";
+
+    static void Vykditioperacija(int x, int y, SkaiciuOperacija operacija)
+    {
+        Console.WriteLine(operacija(x, y));
+    }
+
+    static int Padvigubinti(int x) => x * 2; //skaiciuSarasoOperacija
+    static int PakeltiKvadratu(int x) => x*x;
+    static int Mod10(int x) => x % 10;
+    static int beNeigiamu(int x) => Math.Abs(x);
+
+    static List<int> PritaikytiSarasui(List<int> saltinis, SkaiciuSarašoOperacija op)
+    {
+        var res = new List<int>(saltinis.Count);
+        foreach (var n in saltinis)
+            res.Add(op(n));
+        return res;
+    }
+    static int Agreguoti(List<int> saltinis, Func<int, int, int> op, int seed)
+    {
+        int acc = seed;
+        foreach (var n in saltinis)
+            acc = op(acc, n);
+        return acc; 
+    }
+
+
+    static void Main()
+    {
+        var tekstai = new List<string> { "Labas", "Pasauli", "C#", "Delegatai" };
+
+        var d1 = new TekstoOperacija(IDidziasias);
+        var d2 = new TekstoOperacija(IMazosiomis);
+        var d3 = new TekstoOperacija(Atbulai);
+
+        // var A = Pritaikyti(tekstai, d1);
+        var A = Pritaikyti(tekstai, IDidziasias);
+        var B = Pritaikyti(tekstai, d2);
+        var C = Pritaikyti(tekstai, d3);
+        var BeBalsiu = Pritaikyti(tekstai, s => new string((s ?? "").ToCharArray().Where(ch => !"AEIOUĄĘĖĮYOUaeiouąęėįyou".Contains(ch)).ToArray()));
+
+        Console.WriteLine("Didžiosiomis:    " + string.Join(", ", A));
+        Console.WriteLine("Mažosiomis:      " + string.Join(", ", B));
+        Console.WriteLine("Žodis Atbulai:   " + string.Join(", ", C));
+        Console.WriteLine(":   " + string.Join(", ", BeBalsiu));
+
+
+        int a = 10, b = 5; // negali būti 0
+
+        var ops = new List<SkaiciuOperacija> { Sudetis, Atimti, Daugyba, Dalyba };
+        foreach (var op in ops)
+        {
+            Vykditioperacija(a, b, op);
+        }
+
+        // SkaiciuOperacija op1 = Sudetis;
+        // SkaiciuOperacija op2 = Atimti;
+        // SkaiciuOperacija op3 = Daugyba;
+        // SkaiciuOperacija op4 = Dalyba;
+
+        // Vykditioperacija(a, b, op1);
+        // Vykditioperacija(a, b, op2);
+        // Vykditioperacija(a, b, op3);
+        // Vykditioperacija(a, b, op4);
+
+        //Vykditioperacija(a, b, (x, y) => $"{x}+{y}= {x + y}");
+
+        var skaičiai = new List<int> { 1, 7, 8, -5, 54, 69, 15, 4654, -456, 12, 363, 415 };
+
+        var x2 = PritaikytiSarasui(skaičiai, Padvigubinti);
+        var kv = PritaikytiSarasui(skaičiai, PakeltiKvadratu);
+        var m10 = PritaikytiSarasui(skaičiai, Mod10);
+        var beminus = PritaikytiSarasui(skaičiai, beNeigiamu);
+        //var beNeigiamu = PritaikytiSarasui(skaičiai, x => Math.Abs(x));
+
+        int suma = Agreguoti(skaičiai, (acc, n) => acc + n, 0);
+        //int sandauga = Agreguoti(skaičiai, (acc, n) => acc * n, 1);
+        long sandauga = Agreguoti(skaičiai, (acc, n) => checked((int)checked((long)acc * n)), 1); 
+        int max = Agreguoti(skaičiai, (acc, n) => Math.Max(acc, n), int.MinValue);
+
+        Console.WriteLine("Visas sarašas: " + string.Join (", ", skaičiai)); 
+        Console.WriteLine("x2:   " + string.Join(", ", x2));
+        Console.WriteLine("^2:   " + string.Join(", ", kv));
+        Console.WriteLine("%10:  " + string.Join(", ", m10));
+        Console.WriteLine("|x|:  " + string.Join(", ", beminus));
+        Console.WriteLine($"Suma: {suma}");
+        Console.WriteLine($"Sandauga: {sandauga}");
+        Console.WriteLine($"Max: {max}");
+    }
+
 }
